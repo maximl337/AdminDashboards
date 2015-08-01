af61220d7ab8e7f551a1f697d87ba4dcb4521b69
cBrahmin <angad_dubey@hotmail.com>
2015-07-31 22:49:51 -0400

    initial

---
+++ database/migrations/2015_08_01_023927_create_templates_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTemplatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('templates', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('templates', function (Blueprint $table) {
+            //
+        });
+    }
+}
