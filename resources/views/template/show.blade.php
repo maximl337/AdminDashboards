b931106f8700f3d3e9b6ec827eb6144656bae064
cBrahmin <angad_dubey@hotmail.com>
2015-08-09 21:59:07 -0400

    added urls

--- resources/views/template/show.blade.php
+++ resources/views/template/show.blade.php
@@ -37,10 +37,10 @@
                         data-currency="CAD" 
                         data-shipping="0" 
                         data-tax="3.50" 
-                        data-callback="{{ url('/') }} . paypal/callback" 
+                        data-callback="{{ url('/') }}/paypal/callback" 
                         data-custom="{{ $template->id . ', single' }}"
                         data-env="sandbox"
-                        data-notify_url="{{ url('/') }} . paypal/ipn"
+                        data-notify_url="{{ url('/') }}/paypal/ipn"
                     ></script>
 
 
