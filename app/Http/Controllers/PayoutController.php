a91b42da493be1d7533e7a560b782928fe5d7b6f
cBrahmin <angad_dubey@hotmail.com>
2015-08-21 21:03:50 -0400

    Paypal sdk 2

--- app/Http/Controllers/PayoutController.php
+++ app/Http/Controllers/PayoutController.php
@@ -40,5 +40,42 @@ class PayoutController extends Controller
                 env('PAYPAL_CLIENT_SECRET')      // ClientSecret
             )
         );
+
+        $payouts = new \PayPal\Api\Payout();
+
+        $senderBatchHeader = new \PayPal\Api\PayoutSenderBatchHeader();
+
+        $senderBatchHeader->setSenderBatchId(uniqid())
+            ->setEmailSubject("You have a Payout!");
+
+    $senderItem = new \PayPal\Api\PayoutItem();
+    $senderItem->setRecipientType('Email')
+        ->setNote('Thanks for your patronage!')
+        ->setReceiver('shirt-supplier-one@gmail.com')
+        ->setSenderItemId("2014031400023")
+        ->setAmount(new \PayPal\Api\Currency('{
+                            "value":"1.0",
+                            "currency":"USD"
+                        }'));
+
+    $payouts->setSenderBatchHeader($senderBatchHeader)
+        ->addItem($senderItem);
+
+
+        $request = clone $payouts;
+
+
+        try {
+        $output = $payouts->createSynchronous($apiContext);
+    } catch (Exception $ex) {
+
+        ResultPrinter::printError("Created Single Synchronous Payout", "Payout", null, $request, $ex);
+        exit(1);
+    }
+
+    ResultPrinter::printResult("Created Single Synchronous Payout", "Payout", $output->getBatchHeader()->getPayoutBatchId(), $request, $output);
+
+    return $output;
+
     }
 }
