f105ef1306c6f0d589af093698d5d9c5ac24b150
cBrahmin <angad_dubey@hotmail.com>
2015-08-21 21:11:33 -0400

    Paypal sdk 3

--- app/Http/Controllers/PayoutController.php
+++ app/Http/Controllers/PayoutController.php
@@ -46,36 +46,42 @@ class PayoutController extends Controller
         $senderBatchHeader = new \PayPal\Api\PayoutSenderBatchHeader();
 
         $senderBatchHeader->setSenderBatchId(uniqid())
-            ->setEmailSubject("You have a Payout!");
+                            ->setEmailSubject("You have a Payout!");
 
-    $senderItem = new \PayPal\Api\PayoutItem();
-    $senderItem->setRecipientType('Email')
-        ->setNote('Thanks for your patronage!')
-        ->setReceiver('shirt-supplier-one@gmail.com')
-        ->setSenderItemId("2014031400023")
-        ->setAmount(new \PayPal\Api\Currency('{
-                            "value":"1.0",
-                            "currency":"USD"
-                        }'));
+        $senderItem = new \PayPal\Api\PayoutItem();
+        $senderItem->setRecipientType('Email')
+                    ->setNote('Thanks for your patronage!')
+                    ->setReceiver('shirt-supplier-one@gmail.com')
+                    ->setSenderItemId("2014031400023")
+                    ->setAmount(new \PayPal\Api\Currency('{
+                                        "value":"1.0",
+                                        "currency":"USD"
+                                    }'));
 
-    $payouts->setSenderBatchHeader($senderBatchHeader)
-        ->addItem($senderItem);
+        $payouts->setSenderBatchHeader($senderBatchHeader)
+                ->addItem($senderItem);
 
 
         $request = clone $payouts;
 
 
         try {
-        $output = $payouts->createSynchronous($apiContext);
-    } catch (Exception $ex) {
+            $output = $payouts->createSynchronous($apiContext);
+        } catch (Exception $ex) {
 
-        ResultPrinter::printError("Created Single Synchronous Payout", "Payout", null, $request, $ex);
-        exit(1);
-    }
+            // ResultPrinter::printError("Created Single Synchronous Payout", "Payout", null, $request, $ex);
+            // exit(1);
 
-    ResultPrinter::printResult("Created Single Synchronous Payout", "Payout", $output->getBatchHeader()->getPayoutBatchId(), $request, $output);
+            return response()->json([
+                    $output, $ex
+                ]);
+        }
 
-    return $output;
+        //ResultPrinter::printResult("Created Single Synchronous Payout", "Payout", $output->getBatchHeader()->getPayoutBatchId(), $request, $output);
+
+
+
+        return $output;
 
     }
 }
