d6ed4ea1174654a11d0f01da7ee2fdcd8479d357
cBrahmin <angad_dubey@hotmail.com>
2015-08-23 16:48:44 -0400

    Payout IoC

--- app/Contracts/Payment.php
+++ app/Contracts/Payment.php
@@ -6,7 +6,7 @@ interface Payment {
 
     public function sendSinglePayment($type, $recipient, $amount, $senderItemId, $currency = 'CAD', $note = '');
 
-    public function sendBatchPayment($type, array $recipientsAndAmountAndSenderItemId, $senderBatchId, $currency = 'CAD', $note = '');
+    public function sendBatchPayment(array $recipientsAndAmountAndSenderItemId, $senderBatchId, $type= 'Email',  $currency = 'CAD', $note = '');
 
     public function getBatchPaymentDetails($batchPaymentId);
 
